89d79c7cd465d0aabf3a70a3583a6de996137eb8
Viky <vigneshvics13@gmail.com>
2025-04-11 19:21:18 +0530

    Update development server port, add axios and json-server dependencies, and refactor App component to fetch data from a local JSON server

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,36 @@
+import { useState, useEffect } from "react";
 import GenerateContainer from "./components/generate_container";
+import axios from "axios";
 
-function App(props) {
-  const containerClass = ["container1", "container2", "container3"]
-  let j = 0;
-  let divItems = [];
-  for (let i = 0; i < props.containers.length; i++) {
-    divItems.push(<GenerateContainer containerObj={props.containers[i]} containerClass={containerClass[j]} />)
-    j++
-    if (j == containerClass.length) {
-      j = 0
-    }
-  }
+function App() {
+  const [containers, setContainers] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/layout")
+      .then((response) => {
+        setContainers(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  const containerClass = ["container1", "container2", "container3"];
+
+  const divItems = containers.map((item, i) => {
+    return (
+      <GenerateContainer
+        containerObj={item}
+        containerClass={containerClass[i % containerClass.length]}
+      />
+    );
+  });
 
   return (
     <div>
       <h1>What's Included In All Access</h1>
-      <div className="mainContainer">
-        {divItems}
-      </div>
+      <div className="mainContainer">{divItems}</div>
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,60 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-const DATA = [
-  {
-    heading: "AlgoExpert",
-    list: [
-      "200 of the best coding interiew questions.",
-      "In-depth video explanations.",
-      "A feature-rich in-browser IDE.",
-      "A feature-rich in-browser IDE. In-depth video explanations. A feature -ric in-browser IDE.",
-    ]
-  },
-  {
-    heading: "SystemsExpert",
-    list: [
-      "200 of the best coding interiew questions.",
-      "In-depth video explanations.",
-      "A feature-rich in-browser IDE.",
-    ]
-  },
-  {
-    heading: "FrontEndExpert",
-    list: [
-      "200 of the best coding interiew questions.",
-      "In-depth video explanations.",
-      "A feature-rich in-browser IDE.",
-    ]
-  },
-  {
-    heading: "FrontEndExpert",
-    list: [
-      "200 of the best coding interiew questions.",
-      "In-depth video explanations.",
-      "A feature-rich in-browser IDE.",
-    ]
-  },
-  {
-    heading: "FrontEndExpert",
-    list: [
-      "200 of the best coding interiew questions.",
-      "In-depth video explanations.",
-      "A feature-rich in-browser IDE.",
-    ]
-  },
-  {
-    heading: "FrontEndExpert",
-    list: [
-      "200 of the best coding interiew questions.",
-      "In-depth video explanations.",
-      "A feature-rich in-browser IDE.",
-    ]
-  }
-]
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App containers={DATA} />
+    <App />
   </StrictMode>,
 )
